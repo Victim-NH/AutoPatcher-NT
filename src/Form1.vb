75894469a3272d9fb7a4a8c11c4c9275d219151b
NosHamlet - Team <39097430+Victim-NH@users.noreply.github.com>
2018-05-10 21:32:32 +0200

    Upload base Project

---
+++ src/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub START_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub DISCORD_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+
+    Private Sub WEBSITE_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+    End Sub
+
+    Private Sub EXIT_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+    End Sub
+End Class
