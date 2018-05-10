cad7caaf77cdddbf3732a0e8d455347fe948c085
NosHamlet - Team <39097430+Victim-NH@users.noreply.github.com>
2018-05-10 21:42:45 +0200

    Inserted discord link and website

--- src/Form1.vb
+++ src/Form1.vb
@@ -8,11 +8,11 @@
     End Sub
 
     Private Sub DISCORD_Click(sender As Object, e As EventArgs) Handles Button2.Click
-
+        Process.Start("https://LINK DISCORD")
     End Sub
 
     Private Sub WEBSITE_Click(sender As Object, e As EventArgs) Handles Button3.Click
-
+        Process.Start("https://LINK WEBSITE")
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
