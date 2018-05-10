3e4b0bd23168e7a330746e21cf08b40463aae3e8
NosHamlet - Team <39097430+Victim-NH@users.noreply.github.com>
2018-05-10 21:40:33 +0200

    Insert Welcome Message

--- src/Form1.vb
+++ src/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        MessageBox.Show("Welcome to NosTale Server")
     End Sub
 
     Private Sub START_Click(sender As Object, e As EventArgs) Handles Button1.Click
